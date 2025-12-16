e32285086af243844651fbf6e42ea2c26f3b85d1
sonchouz <sofianikonorova41@gmail.com>
2025-12-16 13:14:49 +0700

    create saved and login screens

--- app/src/main/java/com/example/matuleapp/MainPageActivity.kt
+++ app/src/main/java/com/example/matuleapp/MainPageActivity.kt
@@ -5,10 +5,12 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.matuleapp.R.id.btnsaved
 import com.example.matuleapp.R.id.main
 
 class MainPageActivity : AppCompatActivity() {
@@ -25,6 +27,11 @@ class MainPageActivity : AppCompatActivity() {
         val btnoutdoor = findViewById<Button>(R.id.outdoor_btn)
         btnoutdoor.setOnClickListener {
             val intent = Intent(this, Catalog::class.java)
+            startActivity(intent)
+        }
+        val btnsaved = findViewById<ImageButton>(R.id.btnsaved)
+        btnsaved.setOnClickListener {
+            val intent = Intent(this, SavedActivity::class.java)
             startActivity(intent)
         }
     }
