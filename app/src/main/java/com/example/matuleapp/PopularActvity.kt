3bf080df0809566b56c30d6a43d4597001e72578
sonchouz <sofianikonorova41@gmail.com>
2025-12-17 10:44:15 +0700

    add again popularactivity

--- app/src/main/java/com/example/matuleapp/PopularActvity.kt
+++ app/src/main/java/com/example/matuleapp/PopularActvity.kt
@@ -1,20 +1,26 @@
 package com.example.matuleapp
-
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class PopularActivity : AppCompatActivity() {
+
+class PopularActvity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_popular)
+        setContentView(R.layout.activity_main_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
     }
 }
